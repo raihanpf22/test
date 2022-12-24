0e08ffe47ff85a5dfa170bcf88d48170a5619361
raihanpf22 <raihanpambagyo@.com>
2022-12-24 17:09:52 +0700

    update 2

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 app.use(express.json())
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! dari server')
 })
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
