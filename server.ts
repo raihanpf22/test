1c205f7f31a0e049b0e8923c8862db9f207d8b71
raihanpf22 <raihanpambagyo@.com>
2022-12-24 17:13:35 +0700

    update

--- server.ts
+++ server.ts
@@ -1,13 +1,16 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import * as dotenv from "dotenv";
 
-const app = express()
-const PORT = process.env.PORT
-app.use(express.json())
-app.get('/', (req, res) => {
-  res.send('Hello World! dari server')
-})
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+console.log(PORT);
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World! dari server");
+});
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
